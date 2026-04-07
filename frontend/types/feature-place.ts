6df8cdde6f1244a1296eec6333a0155ea7e0adcf
pyQT_Hai <102220013@sv1.dut.udn.vn>
2026-04-07 16:27:30 +0700

    add json jp

---
+++ frontend/types/feature-place.ts
@@ -0,0 +1,13 @@
+export interface FeaturePlace {
+    id: string;
+    name: string;
+    address: string;
+    type: string;
+    tags: string[];
+    image: string;
+    shortDescription: string;
+    rating: number;
+    reviewsCount: number;
+    latitude: number;
+    longitude: number;
+}
